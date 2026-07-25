b502a7a1c1d885fe02a618a37e665373e70525f5
adfg9 <1822883109@qq.com>
2026-07-26 01:28:37 +0800

    fix: 修改默认Web端口从1001改为1002（1001被系统保留）

--- gui/server.js
+++ gui/server.js
@@ -1144,7 +1144,7 @@ function killPort(port) {
   } catch {}
 }
 
-function start(port = 1001) {
+function start(port = 1002) {
   killPort(port);
   const server = http.createServer(handleRequest);
   wss = new WebSocketServer({ server });
